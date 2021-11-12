69bfa9576d899699e2595619e8fc91cd96e160ad
Kenyi Hancco <kenyi.hancco@cloudcomputing.com.pe>
2021-11-11 20:50:45 -0500

    Se empieza con la construcion de la interfaz

--- intefaz.php
+++ intefaz.php
@@ -9,11 +9,39 @@
 <body>
     <div class="wrapper">
         <h1>Registrar Estudiantes</h1>
-        <form action="./registrar.php" method="POST">
-            <input name="nombre" type="text" name="nombre" placeholder="Nombre">
-            <input name="email" type="text" name="email" placeholder="Email">
-            <input name="carrera" type="text" name="carrera" placeholder="Carrera">
-            <input type="submit" value="Registrar">
-    </div>    
+        <div class="register-area">
+            <form action="./registrar.php" method="POST">
+                <input name="nombre" type="text" name="nombre" placeholder="Nombre" required>
+                <input name="email" type="text" name="email" placeholder="Email"required>
+                <input name="carrera" type="text" name="carrera" placeholder="Carrera"required>
+                <input type="submit" value="Registrar">
+            </form>
+        </div>
+        <div class="list-area">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Carrera</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $estudiantes = file_get_contents('./estudiantes.json');
+                        $estudiantes = json_decode($estudiantes, true);
+                        foreach($estudiantes as $estudiante){
+                            echo '<tr>';
+                            echo '<td>'.$estudiante['nombre'].'</td>';
+                            echo '<td>'.$estudiante['email'].'</td>';
+                            echo '<td>'.$estudiante['carrera'].'</td>';
+                            echo '</tr>';
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
 </body>
 </html>
--- registrar.php
+++ registrar.php
@@ -13,6 +13,6 @@
     if($resultado){
         echo "<script>alert('REGISTRO EXITOSO');window.location='/database-practice/Practice2/intefaz.php'</script>";
     }else{
-        echo "<script>alert('ERROR AL REGISTRAR'); window..history.go(-1);</script>";
+        echo "<script>alert('ERROR AL REGISTRAR'); window.history.go(-1);</script>";
     }
 ?>
